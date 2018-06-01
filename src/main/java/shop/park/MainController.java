486afca5c72e754d8248122b90f4a824f709b3b6
yeon <bsuyeon16@gmail.com>
2018-06-01 18:33:56 +0900

    공지사항, 자주하는 질문 조회 구현

--- src/main/java/shop/park/MainController.java
+++ src/main/java/shop/park/MainController.java
@@ -5,15 +5,18 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import shop.park.exception.FaqNotFoundException;
+import shop.park.exception.NoticeNotFoundException;
 import shop.park.exception.ProductNotFoundException;
+import shop.park.model.Faq;
+import shop.park.model.Notice;
 import shop.park.model.Products;
+import shop.park.service.NoticeServiceImpl;
 import shop.park.service.ProductServiceImpl;
 
 @RestController
@@ -24,6 +27,9 @@ public class MainController {
 	
 	@Autowired
 	ProductServiceImpl productService;
+	
+	@Autowired
+	NoticeServiceImpl noticeService;
 	
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public List<Products> listAllProducts() throws ProductNotFoundException {
@@ -50,5 +56,59 @@ public class MainController {
 		Products product = productService.selectByProduct(p_code);
 		
 		return product;
+	}
+	
+	@RequestMapping(value = "/notice", method = RequestMethod.GET)
+	public List<Notice> listAllNotices() throws NoticeNotFoundException {
+		log.info("Select All Notices");
+		
+		List<Notice> notices = noticeService.selectAllNotice();
+		
+		return notices;
+	}
+	
+	@RequestMapping(value = "/notice/{n_category}", method = RequestMethod.GET)
+	public List<Notice> getByNoticeCategory(@PathVariable("n_category") String n_category) throws NoticeNotFoundException {
+		log.info("Select Notice by n_category");
+		
+		List<Notice> notice = noticeService.selectByNoticeCategory(n_category);
+		
+		return notice;
+	}
+	
+	@RequestMapping(value = "/notice/{n_no}", method = RequestMethod.GET)
+	public Notice getByNoticeNo(@PathVariable("n_no") long n_no) throws NoticeNotFoundException {
+		log.info("Select Notice by n_no");
+		
+		Notice notice = noticeService.selectByNoticeNo(n_no);
+		
+		return notice;
+	}
+	
+	@RequestMapping(value = "/faq", method = RequestMethod.GET)
+	public List<Faq> listAllFaq() throws FaqNotFoundException {
+		log.info("Select All Faq");
+		
+		List<Faq> faq = noticeService.selectAllFaq();
+		
+		return faq;
+	}
+	
+	@RequestMapping(value = "/faq/{f_category}", method = RequestMethod.GET)
+	public List<Faq> getByFaqCategory(@PathVariable("f_category") String f_category) throws FaqNotFoundException {
+		log.info("Select Faq by f_category");
+		
+		List<Faq> faq = noticeService.selectByFaqCategory(f_category);
+		
+		return faq;
+	}
+	
+	@RequestMapping(value = "/faq/{f_no}", method = RequestMethod.GET)
+	public Faq getByFaqNo(@PathVariable("f_no") long f_no) throws FaqNotFoundException {
+		log.info("Select Faq by f_no");
+		
+		Faq faq = noticeService.selectByFaqNo(f_no);
+		
+		return faq;
 	}
 }
---
+++ src/main/java/shop/park/repository/NoticeMapper.java
@@ -0,0 +1,31 @@
+package shop.park.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import shop.park.model.Faq;
+import shop.park.model.Notice;
+
+@Mapper
+public interface NoticeMapper {
+	
+	@Select("SELECT * FROM tm_notice ORDER BY n_no")
+	public List<Notice> selectAllNotice();
+	
+	@Select("SELECT * FROM tm_notice WHERE n_no=#{n_no}")
+	public Notice selectByNoticeNo(long n_no);
+	
+	@Select("SELECT * FROM tm_notice WHERE n_category=#{n_category}")
+	public List<Notice> selectByNoticeCategory(String n_category);
+	
+	@Select("SELECT * FROM tm_faq ORDER BY f_no")
+	public List<Faq> selectAllFaq();
+	
+	@Select("SELECT * FROM tm_faq WHERE f_no=#{f_no}")
+	public Faq selectByFaqNo(long f_no);
+	
+	@Select("SELECT * FROM tm_faq WHERE f_category=#{f_category}")
+	public List<Faq> selectByFaqCategory(String f_category);
+}
