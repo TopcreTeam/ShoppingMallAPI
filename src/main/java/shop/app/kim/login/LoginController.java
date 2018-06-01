06b17f02bdbc7a2929394a274afaaf7fe8bf2f73
k010928842__@naver.com <k010928842__@naver.com>
2018-06-01 15:07:01 +0900

    로그인 구현중 필요파일들

--- src/main/java/shop/app/kim/login/LoginController.java
+++ src/main/java/shop/app/kim/login/LoginController.java
@@ -1,11 +1,33 @@
-package shop.kim;
+package shop.app.kim.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import shop.app.kim.login.model.User;
+import shop.app.kim.login.model.Values;
+import shop.app.kim.login.service.LoginService;
 
-@RequestMapping("/toma")
+
+//@RequestMapping("/toma/login")
 @RestController
 public class LoginController {
-
+	
+	@Autowired
+	private LoginService loginService;
+	
+	@PostMapping("/")
+	public int login(Values values) { //Values : 앵귤러에서 던진 id/pw
+		int result = loginService.authenticate(values);
+		
+		return result;
+	}
+	@GetMapping("/user/user/user")
+	public User getUser() {
+		System.out.println(loginService.getUser());
+		return null;
+	}
 }
---
+++ src/main/java/shop/app/kim/login/model/Values.java
@@ -0,0 +1,18 @@
+package shop.app.kim.login.model;
+
+public class Values {
+	private String uid;
+	private String upw;
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getUpw() {
+		return upw;
+	}
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+}
