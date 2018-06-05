f2e1a8671a990b2245d92d7aa228f8a65c4e388a
k010928842__@naver.com <k010928842__@naver.com>
2018-06-05 15:08:24 +0900

    login 기능확인

--- src/main/java/shop/kim/login/LoginController.java
+++ src/main/java/shop/kim/login/LoginController.java
@@ -3,13 +3,13 @@ package shop.kim.login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import shop.kim.login.model.User;
 import shop.kim.login.model.Login;
+import shop.kim.login.model.User;
 import shop.kim.login.service.LoginService;
 
 
@@ -22,15 +22,21 @@ public class LoginController {
 	private LoginService loginService;
 	
 	@PostMapping("/")
-	public User login(@PathVariable("login") Login login) { //Login : 앵귤러에서 던진 id/pw
+	public User login(@RequestBody Login login) { //Login : 앵귤러에서 던진 id/pw
+
+		System.out.println("확인");
 		User user = loginService.logincheck(login);
 		if(user==null) {//아이디가 존재하지 않는다
+			System.out.println("널이들어왔다");
 			return null;
 		}else {
 			if(!user.getUpw().equals(login.getUpw())) {//비밀번호가 틀렸다
 				return null;
 			}
 		}
+		System.out.println("Return직전");
+		System.out.println(user.getUname());
+		System.out.println(user.getUaddr1());
 		return user;
 	}
 	@GetMapping("/user/list")
