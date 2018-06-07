109cf578e0b80ff34f1d6ad1dce9f9e977c15141
k010928842__@naver.com <k010928842__@naver.com>
2018-06-07 14:37:28 +0900

    login/cart조회까지

--- src/main/java/shop/kim/order/model/Cart.java
+++ src/main/java/shop/kim/order/model/Cart.java
@@ -5,6 +5,49 @@ public class Cart {
 	private String uid;
 	private String pcode;
 	private int camount;
+	
+	//	수정
+	
+	private String p_img;
+	private int p_sellprice;
+	private String p_name;
+	private String p_kind;
+	private String p_content;
+	
+
+	
+	public String getP_content() {
+		return p_content;
+	}
+	public void setP_content(String p_content) {
+		this.p_content = p_content;
+	}
+	public String getP_kind() {
+		return p_kind;
+	}
+	public void setP_kind(String p_kind) {
+		this.p_kind = p_kind;
+	}
+	public String getP_img() {
+		return p_img;
+	}
+	public void setP_img(String p_img) {
+		this.p_img = p_img;
+	}
+	public int getP_sellprice() {
+		return p_sellprice;
+	}
+	public void setP_sellprice(int p_sellprice) {
+		this.p_sellprice = p_sellprice;
+	}
+	public String getP_name() {
+		return p_name;
+	}
+	public void setP_name(String p_name) {
+		this.p_name = p_name;
+	}
+	
+	// 수정끝
 	public int getCno() {
 		return cno;
 	}
