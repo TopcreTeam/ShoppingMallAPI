09ff757391f51aced2af2925840591c6bf930998
JasonChoi <ccc11203@gmail.com>
2018-06-15 20:03:15 +0900

    직원관리매핑 추가 -최성찬

--- src/main/java/shop/choi/admin/service/AdminService.java
+++ src/main/java/shop/choi/admin/service/AdminService.java
@@ -1,5 +1,7 @@
 package shop.choi.admin.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import shop.choi.admin.model.Admin;
@@ -7,4 +9,8 @@ import shop.choi.admin.model.Admin;
 @Service
 public interface AdminService {
 	public Admin adminLoginCheck(Admin admin);
+	public List<Admin> getAdminList();
+	public int createAdmin(Admin admin);
+	public int updateAdmin(Admin admin);
+	public int deleteAdmin(String a_id);
 }
--- src/main/java/shop/choi/admin/service/AdminServiceImpl.java
+++ src/main/java/shop/choi/admin/service/AdminServiceImpl.java
@@ -1,5 +1,7 @@
 package shop.choi.admin.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,27 @@ public class AdminServiceImpl implements AdminService {
 	@Override
 	public Admin adminLoginCheck(Admin admin) {
 		return adminMapper.adminLoginCheck(admin.getA_id());
+	}
+
+	@Override
+	public List<Admin> getAdminList() {
+		List<Admin> adminList = adminMapper.selectAllAdmin();
+		return adminList;
+	}
+
+	@Override
+	public int createAdmin(Admin admin) {
+		return adminMapper.insertAdmin(admin);
+	}
+
+	@Override
+	public int deleteAdmin(String a_id) {
+		return adminMapper.deleteAdmin(a_id);
+	}
+
+	@Override
+	public int updateAdmin(Admin admin) {
+		return adminMapper.updateAdmin(admin);
 	}
 	
 	
