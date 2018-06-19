c26583514d4ef47da4f21d29bd90bea117ece481
JasonChoi <ccc11203@gmail.com>
2018-06-19 11:20:48 +0900

    File uploading 구현 -최성찬

---
+++ src/main/java/shop/choi/fileupload/exception/MyFileNotFoundException.java
@@ -0,0 +1,12 @@
+package shop.choi.fileupload.exception;
+
+
+public class MyFileNotFoundException extends RuntimeException {
+    public MyFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public MyFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
