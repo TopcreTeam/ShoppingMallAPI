6d3e1df41c58c8f6ad239f2e4dd634a70ba4b5a4
k010928842__@naver.com <k010928842__@naver.com>
2018-06-27 11:41:55 +0900

    동현 관리자 작업중 주문삭제, 상태 업데이트 가능

--- src/main/java/shop/kim/webSquare/kimWebsquareController.java
+++ src/main/java/shop/kim/webSquare/kimWebsquareController.java
@@ -1,6 +1,5 @@
 package shop.kim.webSquare;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -87,6 +86,19 @@ public class kimWebsquareController {
 				orderService.orderDetailDelete(removeOno);//참조키를 가진 order_detail테이블 행 먼저 delete
 				orderService.aOrderDelete(removeOno);//order_detail 행 delete
 			}
-			
+		}
+		@PutMapping("/updateOrder")
+		public void updateOrder(@RequestBody Map order) {
+			ArrayList orderList = (ArrayList) order.get("orderList");
+			Map orderMap;
+			Order odr=new Order();
+			for(int i=0;i<orderList.size();i++) {
+				orderMap=(Map)orderList.get(i);
+				if(orderMap.get("rowStatus").toString().equals("U")) {
+					odr.setOno((int)orderMap.get("ono"));
+					odr.setOstatus(orderMap.get("ostatus").toString().charAt(0));
+					orderService.orderUpdate(odr);
+				}
+			}
 		}
 }
